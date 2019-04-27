08ad916c99612aae4ea665e558f5b68f42f9a9e0
Chase Harrison <ChaseH88@gmail.com>
2019-04-27 14:12:07 -0500

    Updated

--- src/components/styled-components.js
+++ src/components/styled-components.js
@@ -11,34 +11,34 @@ export const Container = styled.section`
 `;
 
 export const Button = styled.button`
-    background: transparent;
-    padding: .4vw 2vw;
-    margin: 1vw 0 0;
-    font-size: 1vw;
-    text-transform: uppercase;
-    border: 2px solid #bd0a0f;
-    cursor: pointer;
-    transition: all 200ms ease;
-    &:hover{
-      background: #bd0a0f;
-      color: #fff;
-    }
+  background: transparent;
+  padding: .4vw 2vw;
+  margin: 1vw 0 0;
+  font-size: 1vw;
+  text-transform: uppercase;
+  border: 2px solid #bd0a0f;
+  cursor: pointer;
+  transition: all 200ms ease;
+  &:hover{
+    background: #bd0a0f;
+    color: #fff;
+  }
 `;
 
 export const LottoBall = styled.div`
-height: 5vw;
-width: 5vw;
-display: inline-flex;
-margin: 0 5px;
-align-items: center;
-justify-content: center;
-border: 1px solid #eee;
-border-radius: 50%;
-overflow: hidden;
-& span {
-  font-weight: 900;
-  display: block;
-  font-size: 2.75vw
+  height: 5vw;
+  width: 5vw;
+  display: inline-flex;
+  margin: 0 5px;
+  align-items: center;
+  justify-content: center;
+  border: 1px solid #eee;
+  border-radius: 50%;
+  overflow: hidden;
+  & span {
+    font-weight: 900;
+    display: block;
+    font-size: 2.75vw
 }
 background: radial-gradient(ellipse at center, rgba(242,242,242,1) 0%,rgba(185,185,185,1) 100%);
 /* background: radial-gradient(ellipse at center, rgba(217,0,1,1) 0%,rgba(168,17,25,1) 100%); */
